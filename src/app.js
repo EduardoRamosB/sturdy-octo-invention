8d48e13d66b6e69373c8c7a946bc2ec9559aaa38
lalo <eduardo.ramos.b1@gmail.com>
2023-12-04 22:48:33 -0500

    Add Error page

--- src/app.js
+++ src/app.js
@@ -10,23 +10,30 @@ const app = express()
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Static dir to serve
 app.use(express.static(publicDirectoryPath))
+
 
 app.get('', (req, res) => {
   res.render('index', { title: 'Weather App', name: 'Eduardo H. Ramos' })
 })
 
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about', { title: 'About', name: 'Eduardo H. Ramos' })
 })
 
 app.get('/help', (req, res) => {
-  res.render('help')
+  res.render('help', { title: 'Help', name: 'Eduardo H. Ramos' })
+})
+
+app.get('*', (req, res) => {
+  res.render('404')
 })
 
 app.listen(3000, () => {
