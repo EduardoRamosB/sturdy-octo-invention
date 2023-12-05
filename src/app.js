97d47a3c9abb53c0a02a5ae3803990f1655608ea
lalo <eduardo.ramos.b1@gmail.com>
2023-12-04 22:12:00 -0500

    Move and change directories names

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Static dir to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
